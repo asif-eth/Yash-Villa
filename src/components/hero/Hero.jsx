a34a29dab4c53b1e6ec982898cc688169fe51889
asifkhan <khanasifxyz@gmail.com>
2023-09-15 18:21:48 +0530

    Changed Video Codecs To Test

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Hero.css';
 // import yashVilla from "../../assets/yashVilla.mp4"
-import bgVideo from "../../assets/bgVideo.mp4"
 import HomeIcon from '@mui/icons-material/Home';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
@@ -12,19 +11,25 @@ const Hero = () => {
     <>
       <main className="bgContainer">
         <div className="homeIcon">
-          <HomeIcon style={{fontSize: "3rem", color: "#FFFFFF"}} />
-          <InstagramIcon style={{fontSize: "3rem", color: "#FFFFFF"}} />
-          <WhatsAppIcon style={{fontSize: "3rem", color:"#FFFFFF"}} />
+          <HomeIcon style={{ fontSize: "3rem", color: "#FFFFFF" }} />
+          <InstagramIcon style={{ fontSize: "3rem", color: "#FFFFFF" }} />
+          <WhatsAppIcon style={{ fontSize: "3rem", color: "#FFFFFF" }} />
           <button className="enquireBtn">Enquire Now</button>
         </div>
         <div className="header">
           <h1>Yash Villa</h1>
           {/* <p>Unlock Your Brand's Radiance</p> */}
         </div>
-        <video className='bgVideo' src={bgVideo} autoPlay muted loop playsinline></video>
+
+        <video className="bgVideo" autoPlay loop muted playsInline preload='auto'>
+          {/* <source src="/yashVilla.mp4" type="video/mp4" /> */}
+          <source src="/yashVillah264.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
       </main>
     </>
-  )
+  );
 }
 
 export default Hero
